7e05070623c58c793e1de36a2259e165141e75e0
Ion Spanos <42875202+ispanos@users.noreply.github.com>
2024-01-28 20:40:25 +0200

    fix: Correct skel.d error
    https://github.com/ublue-os/bluefin/commit/e116567a538256b52ce448c51a2ed20fd4bfc4c5

--- config/scripts/bleufin-disable-yafti.sh
+++ config/scripts/bleufin-disable-yafti.sh
@@ -3,6 +3,6 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-rm /usr/etc/skel.d/.config/autostart/bluefin-firstboot.desktop
+rm /usr/etc/skel/.config/autostart/bluefin-firstboot.desktop
 rm /usr/etc/yafti.yml
 rm /usr/etc/profile.d/bluefin-firstboot.sh
