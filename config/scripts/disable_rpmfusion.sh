9166724b90528696d453318bd45b64091f3bb11a
ispanos <yiannisvspanos@gmail.com>
2024-05-07 04:20:13 +0300

    format: add comment

--- config/scripts/disable_rpmfusion.sh
+++ config/scripts/disable_rpmfusion.sh
@@ -3,6 +3,7 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
+# https://github.com/ublue-os/bluefin/commit/b9cf82c3fa5760b6e29308ce4da6a048feeb9cf9
 for i in /etc/yum.repos.d/rpmfusion-*; do
     sed -i 's@enabled=1@enabled=0@g' "$i"
 done
--- config/scripts/enable_rpmfusion.sh
+++ config/scripts/enable_rpmfusion.sh
@@ -4,6 +4,7 @@
 set -oue pipefail
 
 # Renenable RPMFusion
+# https://github.com/ublue-os/bluefin/commit/b9cf82c3fa5760b6e29308ce4da6a048feeb9cf9
 sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-{,non}free.repo
 sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-{,non}free-updates.repo
 sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-{,non}free-updates-testing.repo
