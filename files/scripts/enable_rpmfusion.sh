4ef65fd0b08f0ca2401a4cceef0483f4d884fa29
ispanos <yiannisvspanos@gmail.com>
2024-10-07 02:52:19 +0200

    chore: install RPMFusion now that it is removed from bluefin

--- files/scripts/enable_rpmfusion.sh
+++ files/scripts/enable_rpmfusion.sh
@@ -3,8 +3,13 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-# # Renenable RPMFusion
-# # https://github.com/ublue-os/bluefin/commit/b9cf82c3fa5760b6e29308ce4da6a048feeb9cf9
-# sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-{,non}free.repo
-# sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-{,non}free-updates.repo
-# sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-{,non}free-updates-testing.repo
+# Enable RPMFusion
+# https://github.com/ublue-os/bluefin/commit/b9cf82c3fa5760b6e29308ce4da6a048feeb9cf9
+# https://github.com/ublue-os/bluefin/pull/1748/files
+sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-{,non}free.repo
+sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-{,non}free-updates.repo
+sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-{,non}free-updates-testing.repo
+
+rpm-ostree install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
