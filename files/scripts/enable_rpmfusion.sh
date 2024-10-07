12bce69582a0c3908e5531ab91255e5b4d64de18
ispanos <yiannisvspanos@gmail.com>
2024-10-07 02:52:56 +0200

    fix: remove sed commands

--- files/scripts/enable_rpmfusion.sh
+++ files/scripts/enable_rpmfusion.sh
@@ -6,9 +6,6 @@ set -oue pipefail
 # Enable RPMFusion
 # https://github.com/ublue-os/bluefin/commit/b9cf82c3fa5760b6e29308ce4da6a048feeb9cf9
 # https://github.com/ublue-os/bluefin/pull/1748/files
-sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-{,non}free.repo
-sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-{,non}free-updates.repo
-sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-{,non}free-updates-testing.repo
 
 rpm-ostree install \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
