7446555724e6063484d76686d570ee379426f11f
Yiannis Spanos <yiannisvspanos@gmail.com>
2025-04-08 01:19:01 +0300

    feat: remove yafti

--- files/scripts/bleufin-disable-yafti.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-rm /etc/skel/.config/autostart/bluefin-firstboot.desktop
-rm /etc/yafti.yml
-rm /etc/profile.d/bluefin-firstboot.sh
