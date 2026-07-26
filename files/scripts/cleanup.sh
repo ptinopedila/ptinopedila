8b9502ecdd2b3be31261fcc28ff8156954772f50
Yiannis Spanos <yiannisvspanos@gmail.com>
2026-07-27 01:02:53 +0300

    chore: cleanup comments

--- files/scripts/cleanup.sh
+++ files/scripts/cleanup.sh
@@ -19,8 +19,6 @@ for file in "${icons_to_hide[@]}"; do
   fi
 done
 
-# Unhide desktop icons
-
 icons_to_unhide=(
   "/usr/share/applications/htop.desktop"
   "/usr/share/applications/gnome-system-monitor.desktop"
@@ -28,7 +26,7 @@ icons_to_unhide=(
   "/usr/share/applications/org.gnome.Terminal.desktop"
 )
 
-# Loop through each file to hide the icons
+# Loop through each file to unhide the icons
 for file in "${icons_to_unhide[@]}"; do
   if [[ -f $file ]]; then
     sed -i '/Hidden=true/d' "$file"
@@ -36,17 +34,6 @@ for file in "${icons_to_unhide[@]}"; do
 done
 
 # Disable all COPRs and RPM Fusion Repos
-
-# # Done upstream
-# sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
-# sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/tailscale.repo
-# sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/charm.repo
-# sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ublue-os-bling-fedora-"${FEDORA_MAJOR_VERSION}".repo
-# sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo
-# sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA_MAJOR_VERSION}".repo
-# sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
-# sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
-
 if [ -f /etc/yum.repos.d/fedora-coreos-pool.repo ]; then
     sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-coreos-pool.repo
 fi
